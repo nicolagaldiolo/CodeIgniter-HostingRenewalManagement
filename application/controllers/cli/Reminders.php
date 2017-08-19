2f6ce0590e392d80e1bb3e5a1d206a7eae95e2cf
Nicola Galdiolo <galdiolo.nicola@gmail.com>
2017-08-19 21:50:50 +0200

    reminder config e style.css file optimizatiion

--- application/controllers/cli/Reminders.php
+++ application/controllers/cli/Reminders.php
@@ -15,6 +15,7 @@ class Reminders extends CI_Controller {
     // faccio un giro sugli utenti e invio la mail di promemoria per ogni utente
     $users = $this->ion_auth->users()->result();
     if(!empty($users)){
+      
       foreach($users as $user){
         //echo '<pre>', var_dump($user), '</pre>';
         $userId = $user->id;
@@ -34,7 +35,7 @@ class Reminders extends CI_Controller {
         $this->email->set_mailtype("html");
         $this->email->set_newline("\r\n");
         $this->email->to($user->email);
-        $this->email->from("galdiolo.nicola@gmail.com");
+        $this->email->from('galdiolo.nicola@gmail.com','Hosting manager');
         $this->email->subject("Promemoria Servizi in scadenza {$month}");
         $this->email->message($this->load->view('email_template/reminder', $data, true));
         $this->email->send();
