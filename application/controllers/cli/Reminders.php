81388884f87ee6e4b2da57123d8a66a817753172
Nicola Galdiolo <galdiolo.nicola@gmail.com>
2017-08-20 01:00:27 +0200

    bugfix user inactive in reminders file

--- application/controllers/cli/Reminders.php
+++ application/controllers/cli/Reminders.php
@@ -18,27 +18,29 @@ class Reminders extends CI_Controller {
       
       foreach($users as $user){
         //echo '<pre>', var_dump($user), '</pre>';
-        $userId = $user->id;
+        if($user->active === "1"){
+          $userId = $user->id;
 
-        // Estraggo tutti i domini (legati all'utente) scaduti il mese scorso non acora seganti come pagati
-        $data['domainsExpired'] = $this->reminders_model->getService($userId, TRUE);
-        
-        // Estraggo tutti i domini in scadenza (legati all'utente)
-        $data['domainsCurrent'] = $this->reminders_model->getService($userId, FALSE);
+          // Estraggo tutti i domini (legati all'utente) scaduti il mese scorso non acora seganti come pagati
+          $data['domainsExpired'] = $this->reminders_model->getService($userId, TRUE);
+          
+          // Estraggo tutti i domini in scadenza (legati all'utente)
+          $data['domainsCurrent'] = $this->reminders_model->getService($userId, FALSE);
 
-        $month = "";
-        if(!empty($data['domainsCurrent'])){
-          $month="mese ( " . date("F Y", strtotime($data['domainsCurrent'][0]->renewal) ) . " )";
+          $month = "";
+          if(!empty($data['domainsCurrent'])){
+            $month="mese ( " . date("F Y", strtotime($data['domainsCurrent'][0]->renewal) ) . " )";
+          }
+
+          // preparo la mail di promemoria
+          $this->email->set_mailtype("html");
+          $this->email->set_newline("\r\n");
+          $this->email->to($user->email);
+          $this->email->from('galdiolo.nicola@gmail.com','Hosting manager');
+          $this->email->subject("Promemoria Servizi in scadenza {$month}");
+          $this->email->message($this->load->view('email_template/reminder', $data, true));
+          $this->email->send();
         }
-
-        // preparo la mail di promemoria
-        $this->email->set_mailtype("html");
-        $this->email->set_newline("\r\n");
-        $this->email->to($user->email);
-        $this->email->from('galdiolo.nicola@gmail.com','Hosting manager');
-        $this->email->subject("Promemoria Servizi in scadenza {$month}");
-        $this->email->message($this->load->view('email_template/reminder', $data, true));
-        $this->email->send();
 
       }
       // aggiorno la data di scadenza (+1 anno) dei domini scaduti il mese scorso
